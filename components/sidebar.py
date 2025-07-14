3bcc548ca42fac0e9fdff5e78a71468112a59991
roufy0 <r-a-h-f-11@hotmail.com>
2025-07-15 01:16:02 +0300

    Initial commit for RAG-based Saudi law QA agent

--- components/sidebar.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-def render_sidebar():
-    # Inject external CSS
-    with open("Style/custom.css") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    st.sidebar.write("hello")
-    
